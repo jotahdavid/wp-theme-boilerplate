32819fab73a8407145aa68b892224dda7d631f70
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-04-28 10:36:51 -0300

    chore: setup gulp with sass and browser-sync

--- inc/functions/setup-theme.php
+++ inc/functions/setup-theme.php
@@ -5,3 +5,8 @@ function add_setup_config() {
   add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'add_setup_config', 0);
+
+function enqueue_assets() {
+  wp_enqueue_style('main-style', get_template_directory_uri() . '/assets/css/main.css', []);
+}
+add_action('wp_enqueue_scripts', 'enqueue_assets');
