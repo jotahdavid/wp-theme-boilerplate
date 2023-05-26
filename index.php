30562984d207c2145ff29eace317e4a0123e1bd3
Ygor Combi <ycombi349@gmail.com>
2023-05-26 13:44:50 -0300

    feat: aside html

--- index.php
+++ index.php
@@ -7,6 +7,37 @@
         <?php the_content(); ?>
         oioioioi
       </article>
+      <aside id="aside-right">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/flag-black.png" alt="Ícone - ">
+                <h2>Começando com a ADVBOX</h2>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/configuracoes.png" alt="Ícone - ">
+                <h2>Configurações</h2>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/meu-painel.png" alt="Ícone - ">
+                <h2>Meu Painel</h2>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/pessoas.png" alt="Ícone - ">
+                <h2>Pessoas</h2>
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+      </aside>
     <?php endwhile; ?>
   <?php endif; ?>
 </main>
