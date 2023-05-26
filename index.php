f452198b8428f7364db9fb8ed9e13571dbb6fdd9
Ygor Combi <ycombi349@gmail.com>
2023-05-26 19:30:29 -0300

    feat: aside left

--- index.php
+++ index.php
@@ -1,45 +1,47 @@
 <?php get_header(); ?>
 
-<main id="post" class="main container grid-6" role="main">
+<main id="post" class="main container" role="main">
   <?php if (have_posts()): ?>
     <?php while (have_posts()): the_post(); ?>
-      <section <?php post_class(); ?>>
-      <aside id="aside-right">
-        <nav>
-          <ul>
-            <li class="active">
-              <a href="#">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/flag-black.png" alt="Ícone - ">
-                <h2 class="h2">Começando com a ADVBOX</h2>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/configuracoes.png" alt="Ícone - ">
-                <h2 class="h2">Configurações</h2>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/meu-painel.png" alt="Ícone - ">
-                <h2 class="h2">Meu Painel</h2>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/pessoas.png" alt="Ícone - ">
-                <h2 class="h2">Pessoas</h2>
-              </a>
-            </li>
-          </ul>
-        </nav>
+      <section class="grid-6" <?php post_class(); ?>>
+        <aside id="aside-right">
+          <nav>
+            <ul class="ul">
+              <li class="active">
+                <a href="#">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/flag-black.png" alt="Ícone - ">
+                  <h2 class="h2">Começando com a ADVBOX</h2>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/configuracoes.png" alt="Ícone - ">
+                  <h2 class="h2">Configurações</h2>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/meu-painel.png" alt="Ícone - ">
+                  <h2 class="h2">Meu Painel</h2>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/pessoas.png" alt="Ícone - ">
+                  <h2 class="h2">Pessoas</h2>
+                </a>
+              </li>
+            </ul>
+          </nav>
 
-      </aside>
+        </aside>
 
-        <article><?php the_content(); ?></article>
-      <aside id="aside-left"></aside>
+          <article id="article-content-advbox">
+            <?php the_content(); ?>
+          </article>
+
+        <aside id="aside-left"></aside>
       </section>
-
     <?php endwhile; ?>
   <?php endif; ?>
 </main>
