35a7ea880f04ecfd5635a1eac9758f090379bf49
Ygor Combi <ycombi349@gmail.com>
2023-05-26 14:04:05 -0300

    feat: html sctrute in post

--- index.php
+++ index.php
@@ -1,43 +1,45 @@
 <?php get_header(); ?>
 
-<main class="main" role="main">
+<main id="post" class="main container grid-6" role="main">
   <?php if (have_posts()): ?>
     <?php while (have_posts()): the_post(); ?>
-      <article <?php post_class(); ?>>
-        <?php the_content(); ?>
-        oioioioi
-      </article>
+      <section <?php post_class(); ?>>
       <aside id="aside-right">
         <nav>
           <ul>
             <li>
               <a href="#">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/flag-black.png" alt="Ícone - ">
-                <h2>Começando com a ADVBOX</h2>
+                <h2 class="h2">Começando com a ADVBOX</h2>
               </a>
             </li>
             <li>
               <a href="#">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/configuracoes.png" alt="Ícone - ">
-                <h2>Configurações</h2>
+                <h2 class="h2">Configurações</h2>
               </a>
             </li>
             <li>
               <a href="#">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/meu-painel.png" alt="Ícone - ">
-                <h2>Meu Painel</h2>
+                <h2 class="h2">Meu Painel</h2>
               </a>
             </li>
             <li>
               <a href="#">
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/nav/pessoas.png" alt="Ícone - ">
-                <h2>Pessoas</h2>
+                <h2 class="h2">Pessoas</h2>
               </a>
             </li>
           </ul>
         </nav>
 
       </aside>
+
+        <article><?php the_content(); ?></article>
+      <aside id="aside-left"></aside>
+      </section>
+
     <?php endwhile; ?>
   <?php endif; ?>
 </main>
